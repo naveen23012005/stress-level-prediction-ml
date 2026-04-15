52a3e2b8fcd87e934be27ed765871c40b0b46df0
naveen23012005 <naveenchalla341@gmail.com>
2026-04-15 14:38:43 +0530

    vvvvv

---
+++ tests/test_model.py
@@ -0,0 +1,23 @@
+import joblib
+from sklearn.metrics import r2_score
+import numpy as np
+
+def test_model_performance():
+    model = joblib.load("model/model.pkl")
+
+    # Example test data (replace with real validation data)
+    X_test = np.array([[55, 'Male', 'Teacher', 9.88, 37, 6.81, 4.08, 4, 11, 40, 10.0,
+        395.20000000000005, 27.7848],
+       [37, 'Female', 'Doctor', 9.42, 97, 4.48, 2.94, 3, 99, 197, 10.0,
+        1855.74, 13.1712],
+       [39, 'Female', 'Software Engineer', 1.07, 69, 6.38, 8.66, 3, 29,
+        182, 1.0, 194.74, 55.2508]])
+    y_test = np.array([10. , 10. ,  2.3])
+
+    y_pred = model.predict(X_test)
+
+    acc = r2_score(y_test,y_pred)
+
+    print("Model Accuracy:", acc)
+
+    assert acc >= 0.80
